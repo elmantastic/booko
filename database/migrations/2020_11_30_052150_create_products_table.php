7944820d23fab1b10cfacbdd365d7c19877523a0
elmantastic <elmantastic@gmail.com>
2020-11-30 12:24:24 +0700

    add migration for products

---
+++ database/migrations/2020_11_30_052150_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('image');
+            $table->string('pengarang');
+            $table->string('penerbit');
+            $table->integer('tahun');
+            $table->string('kategori');
+            $table->text('deskripsi');
+            $table->integer('halaman');
+            $table->integer('harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
