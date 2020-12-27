71d6434418f9bb0e6e531fb41c18ac4891e0988f
elmantastic <elmantastic@gmail.com>
2020-12-27 21:33:08 +0700

    Fixed Login and Register Pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,15 +3,15 @@
 @section('content')
 <div class="">
     <div class="row login-screen">
-        <div class="col-sm-4 text-center img-login-sm align-self-center" style="padding: 3%; margin: auto; height: 100%">
-            <div class="container">
-            <p class="h4 mb-4 text-left font-weight-bold">{{ __('Login') }}</p>
+        <div class="col-sm-4 text-center img-login-sm align-self-center">
+            <div class="container ">
+            <p class="h2 mb-4 text-left font-weight-bold">{{ __('Login') }}</p>
             <p class="text-left">Login first before you continue to main page</p>
 
             <form method="POST" action="{{ route('login') }}" style="padding: 0;">
                 @csrf
                 <label for="email" class="in text-white" style="color: #495057; margin-right: 100%;">Email</label>
-                <input id="email" type="email" class="form-control mb-4 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus style=" border-radius: 25px">
+                <input id="email" type="email" class="form-control mb-4 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="{{ __('you@booko.com') }}" autofocus style=" border-radius: 25px">
                 @error('email')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
@@ -19,7 +19,7 @@
                 @enderror
 
                 <label for="password" class="in text-white" style="color: #495057; margin-right: 100%;">Password</label>
-                <input id="password" type="password" class="form-control mb-4 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" style="border-radius: 25px">
+                <input id="password" type="password" class="form-control mb-4 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="{{ __('Password') }}"  style="border-radius: 25px">
                 @error('password')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
@@ -36,12 +36,12 @@
 
                 <div class="form-group row mb-0">
                     <div class="col">
-                        <button type="submit" class="btn btn-primary btn-block" style="border-radius: 25px; margin-top: 2%">
+                        <button type="submit" class="btn btn-primary btn-lg btn-block" style="border-radius: 25px; margin-top: 2%">
                             {{ __('Login') }}
                         </button>
 
                         @if (Route::has('password.request'))
-                            <a class="btn btn-link text-success" href="{{ route('password.request') }}">
+                            <a class="btn btn-link text-white" href="{{ route('password.request') }}">
                                 {{ __('Forgot Your Password?') }}
                             </a>
                         @endif
@@ -50,15 +50,15 @@
             </form>
             </div>
         </div>
-            <div class="text-center col center img-login-lg" style="padding: 2%; background-image: url('{{ asset('assets/images/Login/booksWallpaper.jpg')}}'); background-size:cover; background-repeat: no-repeat;"> 
-                    <div class="mt-5" style="margin: auto 0;">
+            <div class="text-center col center img-login-lg" style="padding: 2%; background-image: url('{{ asset('assets/images/Login/booksWallpaper.jpg')}}'); background-size:cover; background-repeat: no-repeat; "> 
+            </div>
+            <div class="text-over-img-login">
                     <i class="fa fa-user-circle fa-10x mb-4" aria-hidden="true" style="color: white; margin-top: 5%;"></i>
-                    <h2 class="account-text">Create Your Account</h2>
-                    </div>
-                    <h4 class="account-description">Signup to create an account</h4> 
-                    <a class="btn btn-lg btn-primary btn-block text-white sign mt-5" href="{{ route('register') }}" type="submit" style="border-radius: 25px;width: 80%;margin-top: 2%; border-radius: 25px; width: 25%; margin: auto">{{ __('SIGN UP') }}</a>
-                    </div>
-</div>
+                    <h1 class="account-text font-weight-bold">Create Your Account</h1>
+                    <h3 class="account-description">Signup to create an account</h3> 
+                    <a class="btn btn-lg btn-primary btn-block text-white sign mt-5" href="{{ route('register') }}" type="submit" style="border-radius: 25px;">{{ __('SIGN UP') }}</a>
+                </div>
+    </div>
 </div>
 @endsection
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -76,8 +76,69 @@
 </div>
  -->
 
+ <div class="">
+    <div class="row login-screen">
+        <div class="text-center col center img-register-lg" style="padding: 2%; background-image: url('{{ asset('assets/images/Login/booksWallpaper.jpg')}}'); background-size:cover; background-repeat: no-repeat; "> 
+        </div>
+        <div class="text-over-img-register">
+                <i class="fa fa-user-circle fa-10x mb-4" aria-hidden="true" style="color: white; margin-top: 5%;"></i>
+                <h1 class="account-text font-weight-bold">Already have account?</h1>
+                <h3 class="account-description">Sign in now</h3> 
+                <a class="btn btn-lg btn-primary btn-block text-white sign mt-5" href="{{ route('login') }}" type="submit" style="border-radius: 25px;">{{ __('SIGN IN') }}</a>
+        </div>
+        <div class="col-sm-4 text-center img-register-sm align-self-center">
+            <div class="container ">
+            <p class="h2 mb-4 text-left font-weight-bold">{{ __('Register') }}</p>
+            <p class="text-left">Sign up now to get access to buy all our product</p>
 
-<link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css" rel="stylesheet">
+            <form method="POST" action="{{ route('register') }}" style="padding: 0;">
+                @csrf
+                <label for="name" class="in text-white" style="color: #495057; margin-right: 100%;">Name</label>
+                <div class="form-group" style="margin-bottom: 25px;">
+                    <input type="text" class="form-control item @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="{{ __('Your Name') }}" style="border-radius: 25px">
+                    @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <label for="email" class="in text-white" style="color: #495057; margin-right: 100%;">Email</label>
+                <div class="form-group"  style="margin-bottom: 25px;">
+                    <input type="email" class="form-control item @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}" required autocomplete="email"placeholder="{{ __('you@booko.com') }}" style="border-radius: 25px">
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <label for="password" class="in text-white" style="color: #495057; margin-right: 100%;">Password</label>
+                <div class="form-group"  style="margin-bottom: 25px;">
+                    <input type="password" class="form-control item  @error('password') is-invalid @enderror" id="password" name="password" required autocomplete="new-password" placeholder="{{ __('Password') }}" style="border-radius: 25px">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div> 
+                <label for="confirmPassword" class="in text-white float-left" style="color: #495057;">Confirm Password</label>
+                <div class="form-group"  style="margin-bottom: 25px;">
+                    <input type="password" class="form-control item" id="password-confirm" name="password_confirmation" required autocomplete="new-password" placeholder="{{ __('Confirm Password') }}" style="border-radius: 25px">
+                </div>
+
+                <div class="form-group" style="border-radius:30px;padding: 5px 5px; font-size: 18px;background-color: #5791ff;border: none;margin-top:50px;">
+                    <button type="submit" class="btn btn-block register" style="color:#ffffff;">{{ __('Register') }}</button>
+                </div>
+            </form>
+            </div>
+        </div>
+        
+    </div>
+</div>
+
+@endsection
+  
+
+<!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css" rel="stylesheet">
 
 <div class="registration-form" style="padding: 50px 0;">
         <form method="POST" action="{{ route('register') }} " style=" background-color: #fff;max-width: 500px;margin: auto;padding: 50px 70px;border-top-left-radius: 30px;border-top-right-radius: 30px;box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.075);">
@@ -122,6 +183,4 @@
                 <button type="submit" class="btn btn-block register" style="color:#ffffff;">{{ __('Register') }}</button>
             </div>
         </form>
-    </div>
-@endsection
-  
+    </div> -->
